1b8308de6f7ec1f88f079e8198818399abd91b5d
dashcaj <158211825+dashcaj@users.noreply.github.com>
2025-01-09 13:58:14 -0500

    y

--- recipe/js/main.js
+++ recipe/js/main.js
@@ -1,3 +1,13 @@
 import '/css/style.css'
 
+//give specific points to each button
+//make sure each button doesnt refresh the page
+//get the points from each question
+//for a certain point range, insert html when click submit
 
+const DOMSelectors = {
+    one: document.getElementById("1"),
+    two: document.getElementById("1"),
+    three: document.getElementById("1"),
+    submit: document.querySelector(".submitbtn"),
+};
