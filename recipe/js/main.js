9ce7431b5ef9ceaae6d43bac07e9566dbff8851d
dashcaj <dustinc7@nycstudents.net>
2025-01-12 22:11:28 -0500

    uj

--- recipe/js/main.js
+++ recipe/js/main.js
@@ -1,16 +1,17 @@
-// Select elements
+
 import '/css/style.css'
 const buttonlist = document.querySelectorAll(".button");
 const submit = document.querySelector(".submitbtn");
 const area = document.querySelector(".area");
 
+
 let score = 0;
 
 
 const styles = [
-  { min: 0, max: 5, description: "very casual NICe" },
+  { min: 0, max: 5, description: "very casual" },
   { min: 6, max: 10, description: "trendy sports" },
-  { min: 11, max: 15, description: "elegant dude huh." },
+  { min: 11, max: 15, description: "elegant" },
   { min: 16, max: Infinity, description: "who are you????" },
 ];
 
@@ -35,12 +36,13 @@ submit.addEventListener("click", function () {
   const html = `
         <div class="results">
             <h2 class="resulttitle">Your Style Quiz Result:</h2>
-            <h4 class="pointdisplay">Points: ${score}</h4>
             <p class="styledescription">${styleDescription}</p>
+            <h4 class="pointdisplay">Points: ${score}</h4>
         </div>
     `;
   area.innerHTML = html; 
 });
+
 
 
 
